1d95302987e68628983d56e48278dd44f864b6a1
Julio Napurí <julionc@gmail.com>
2020-06-07 15:39:17 -0500

    Update Hybridauth 3.3 library.
    - Follow CodeIgniter's PHP Style Guide.
    - Clean up Hybridauth CI Library.
    - Fixed Basic controller integration.
    
    Fixes #3 and #7
    
    Add missing 'include_email' option for Twitter provider
    
    Fix URI

--- application/controllers/Hauth.php
+++ application/controllers/Hauth.php
@@ -4,71 +4,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * Hauth Controller Class
  */
-class Hauth extends CI_Controller {
+class Hauth extends CI_Controller
+{
 
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct()
-  {
-    parent::__construct();
+	/**
+	 * {@inheritdoc}
+	 */
+	public function __construct()
+	{
+		parent::__construct();
 
-    $this->load->helper('url');
-    $this->load->library('hybridauth');
-  }
+		$this->load->helper('url');
+		$this->load->library('hybridauth');
+	}
 
-  /**
-   * {@inheritdoc}
-   */
-  public function index()
-  {
-    // Build a list of enabled providers.
-    $providers = array();
-    foreach ($this->hybridauth->HA->getProviders() as $provider_id => $params)
-    {
-      $providers[] = anchor("hauth/window/{$provider_id}", $provider_id);
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function index()
+	{
+		// Build a list of enabled providers.
+		$providers = array();
+		foreach ($this->hybridauth->HA->getProviders() as $name) {
+			$uri = 'hauth/window/' . strtolower($name);
+			$providers[] = anchor($uri, $name);
+		}
 
-    $this->load->view('hauth/login_widget', array(
-      'providers' => $providers,
-    ));
-  }
+		$this->load->view('hauth/login_widget', array(
+			'providers' => $providers,
+		));
+	}
 
-  /**
-   * Try to authenticate the user with a given provider
-   *
-   * @param string $provider_id Define provider to login
-   */
-  public function window($provider_id)
-  {
-    $params = array(
-      'hauth_return_to' => site_url("hauth/window/{$provider_id}"),
-    );
-    if (isset($_REQUEST['openid_identifier']))
-    {
-      $params['openid_identifier'] = $_REQUEST['openid_identifier'];
-    }
-    try
-    {
-      $adapter = $this->hybridauth->HA->authenticate($provider_id, $params);
-      $profile = $adapter->getUserProfile();
+	/**
+	 * Try to authenticate the user with a given provider
+	 *
+	 * @param string $provider Define provider to login
+	 */
+	public function window($provider)
+	{
+		try {
+			$adapter = $this->hybridauth->HA->authenticate($provider);
+			$profile = $adapter->getUserProfile();
 
-      $this->load->view('hauth/done', array(
-        'profile' => $profile,
-      ));
-    }
-    catch (Exception $e)
-    {
-      show_error($e->getMessage());
-    }
-  }
-
-  /**
-   * Handle the OpenID and OAuth endpoint
-   */
-  public function endpoint()
-  {
-    $this->hybridauth->process();
-  }
-
+			$this->load->view('hauth/done', array(
+				'profile' => $profile,
+			));
+		} catch (Exception $e) {
+			show_error($e->getMessage());
+		}
+	}
 }
--- application/libraries/Hybridauth.php
+++ application/libraries/Hybridauth.php
@@ -4,102 +4,49 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * Hybridauth Class
  */
-class Hybridauth {
+class Hybridauth
+{
 
-  /**
-   * Reference to the Hybrid_Auth object
-   *
-   * @var Hybrid_Auth
-   */
-  public $HA;
+	/**
+	 * Reference to the Hybrid_Auth object
+	 *
+	 * @var Hybridauth\Hybridauth
+	 */
+	public $HA;
 
-  /**
-   * Reference to CodeIgniter instance
-   *
-   * @var CI_Controller
-   */
-  protected $CI;
+	/**
+	 * Reference to CodeIgniter instance
+	 *
+	 * @var CI_Controller
+	 */
+	protected $CI;
 
-  /**
-   * Class constructor
-   *
-   * @param array $config
-   */
-  public function __construct($config = array())
-  {
-    $this->CI =& get_instance();
-    // Load the HA config.
-    if (!$this->CI->load->config('hybridauth'))
-    {
-      log_message('error', 'Hybridauth config does not exist.');
+	/**
+	 * Class constructor
+	 *
+	 * @param array $config
+	 */
+	public function __construct($config = array())
+	{
+		$this->CI = &get_instance();
 
-      return;
-    }
+		// Load the HA config.
+		if (!$this->CI->load->config('hybridauth')) {
+			log_message('error', 'Hybridauth config does not exist.');
 
-    // Get HA config.
-    $config = $this->CI->config->item('hybridauth');
+			return;
+		}
 
-    // Specify base url to HA Controller.
-    $config['base_url'] = $this->CI->config->site_url('index.php/hauth/endpoint');
+		// Get HA config.
+		$config = $this->CI->config->item('hybridauth');
 
-    try
-    {
-      // Load HA library.
-      $this->_init();
+		try {
+			// Initialize Hybrid_Auth.
+			$this->HA = new Hybridauth\Hybridauth($config);
 
-      // Initialize Hybrid_Auth.
-      $this->HA = new Hybrid_Auth($config);
-
-      log_message('info', 'Hybridauth Class is initialized.');
-    }
-    catch (Exception $e)
-    {
-      show_error($e->getMessage());
-    }
-  }
-
-  /**
-   * Process the HA request
-   */
-  public function process()
-  {
-    $this->_init('Hybrid_Endpoint');
-
-    Hybrid_Endpoint::process();
-  }
-
-  /**
-   * Initialize HA library
-   *
-   * @param string $class_name The class name to initialize
-   *
-   * @throws \Exception
-   */
-  protected function _init($class_name = 'Hybrid_Auth')
-  {
-    list($dir, $filename) = explode('_', $class_name);
-
-    if (class_exists($class_name))
-    {
-      // Nothing to do here. Most probably the class is loaded by composer_autoload.
-    }
-    elseif (file_exists(APPPATH . "third_party/hybridauth/hybridauth/{$dir}/{$filename}.php"))
-    {
-      // In case when the library is placed in third_party/hybridauth.
-      require_once APPPATH . "third_party/hybridauth/hybridauth/{$dir}/{$filename}.php";
-    }
-    elseif (file_exists(FCPATH . 'vendor/autoload.php'))
-    {
-      // Finally try to load the given class from CI autoload.
-      require_once FCPATH . 'vendor/autoload.php';
-    }
-
-    if (!class_exists($class_name))
-    {
-      throw new Exception("Could not load the {$class_name} class.");
-    }
-
-    log_message('info', "{$class_name} class is loaded.");
-  }
-
+			log_message('info', 'Hybridauth Class is initialized.');
+		} catch (Exception $e) {
+			show_error($e->getMessage());
+		}
+	}
 }
